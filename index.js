eddb3c4596a42879f84b0495318c1bc353c41849
Jacobo Tabernero <jacobo@xarope.eu>
2014-08-08 19:31:07 -0500

    idea

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-var dot = require('dot');
-var email = require('emailjs');
+'use strict';
+var dot = require('dot'),
+	email = require('emailjs');
+
+
+var isObject = function (obj) {
+	if (typeof obj !== 'object' || obj === null) {
+		return false;
+	}
+	if (obj.length === undefined) {
+		return true;
+	}
+	return false;
+};
+
+var accounts = {},
+	templates = {};
 
 var mailer = {};
 
-mailer.send = function () {};
-mailer.addAccount = function (key, data) {};
-mailer.setTemplate = function (key, data) {};
-mailer.removeTemplate = function (key) {};
+mailer.send = function (account, email, data, callback) {
+	if (!account || typeof account !== 'string' || !accounts[account]) {
+		return callback( 'invalid account' );
+	}
+	if (!email) {
+		return callback( 'invalid email/template' );
+	}
+	if (typeof email === 'string') {
+		email = templates[email];
+	} else if (!isObject( email )) {
+		return callback( 'invalid email/template' );
+	}
+	if (typeof data === 'function') {
+		callback = data;
+	}
+};
+
+mailer.addAccount = function (key, data) {
+	accounts[key] = data;
+};
+
+mailer.setTemplate = function (key, template) {
+	templates[key] = template;
+};
+
+mailer.getTemplate = function (key) {
+	return templates[key];
+};
+
+mailer.removeTemplate = function (key) {
+	delete templates[key];
+};
 
 module.exports = mailer;
